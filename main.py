166b594296d17cbca5a2e4344aa530bfb9a9c133
meliodas0n <mrunalnarayana@gmail.com>
2023-10-08 16:17:13 +0530

    updated main with AppParams from models to run the app with proper type hinting

--- main.py
+++ main.py
@@ -2,13 +2,17 @@
 
 import tkinter as tk
 
+from models import AppParams
 
 class App:
   def __init__(self):
     self.root = tk.Tk()
 
   def run(self, width, height, title, bg="white"):
-    self.w, self.h = width, height
+    if width & height: self.w, self.h = width, height
+    elif width: self.w, self.h = width, 1080
+    elif height: self.w, self.h = 1920, height
+    else: self.w, self.h = 1920, 1080
     self.title = title
     self.root.geometry(f"{self.w}x{self.h}")
     self.root.configure(bg=bg)
@@ -16,6 +20,11 @@ class App:
     self.root.mainloop()
 
 
-if __name__ == "__main__":
+def main():
   app = App()
-  app.run(width=1920, height=1080, title="Main", bg="white")
+  params = AppParams(width=1920, height=1080, title="Main", bg="white")
+  app.run(**params.model_dump())
+
+
+if __name__ == "__main__":
+  main()
